fdd17d7c289442269068ca44e792ab9e5e134190
wilson <wilsonedet96@gmail.com>
2025-07-14 00:24:30 +0100

    modified

--- basic_operations.py
+++ basic_operations.py
@@ -1,8 +1,5 @@
 number1 = 10
 number2 = 5
-sum = number1 + number2
-difference = number1 - number2
-multiply = number1 * number2
-print("Addition of", number1, "and", number2, "is", sum)
-print("Subtraction of", number1, "and", number2, "is", difference)
-print("Multiplication of", number1, "and", number2, "is", multiply)
+print("Addition of", number1, "and", number2, "is", number1 + number2)
+print("Subtraction of", number1, "and", number2, "is", number1 - number2)
+print("Multiplication of", number1, "and", number2, "is", number1 * number2)
