ff9b41a90f0eadb9933d22ca0bf2d59fdbd63f05
wilson <wilsonedet96@gmail.com>
2025-07-20 21:32:49 +0100

    daily reminder

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -1,29 +1,29 @@
 task = input("Enter your task:")
 priority = input("Priority (high/medium/low):")
 time_bound = input("Is it time-bound? (yes/no):")
-
+reminder = f"'{task}' is a {priority} priority task"
 match priority:
     case "high":
         if time_bound == "yes":
-            reminder = f"'{task}' is a {priority} priority task that require immediate attention today!"
-            print(reminder)
+            message = "that require immediate attention today!"
+            print(f"{reminder} {message}")
         elif time_bound == "no":
-            reminder = f"'{task}' is a {priority} priority task. But you have time to finish the task."
-            print(reminder)
+            message = "But you have time to finish the task."
+            print(f"{reminder}. {message}")
     case "medium":
         if time_bound == "yes":
-            reminder = f"'{task}' is a {priority} priority task that requires attention, time is running out."
-            print(reminder)
+            message = "that requires attention, time is running out."
+            print(f"{reminder} {message}")
         elif time_bound == "no":
-            reminder = f"'{task}' is a {priority} priority task. But you can complete it in your free time."
-            print(reminder)
+            message = "But you can complete it in your free time."
+            print(f"{reminder}. {message}")
     case "low":
         if time_bound == "yes":
-            reminder = f"'{task}' is a {priority} priority task. Time is running out!"
-            print(reminder)
+            message = "Time is running out!"
+            print(f"{reminder}. {message}")
         elif time_bound == "no":
-            reminder = f"'{task}' is a {priority} priority task. Consider completing it in your free time."
-            print(reminder)
+            message = "Consider completing it in your free time."
+            print(f"{reminder}. {message}")
     case _:
         reminder = f"incorrect input!"
         print(reminder)
