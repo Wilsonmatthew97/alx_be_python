99a361cdd9bd67c904a6ab83a03781a5afca5f52
wilson <wilsonedet96@gmail.com>
2025-07-27 21:55:40 +0100

    daily reminder

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -6,24 +6,24 @@ match priority:
     case "high":
         if time_bound == "yes":
             message = "that require immediate attention today!"
-            print(f"{reminder} {message}")
+            print(f"Reminder: {reminder} {message}")
         elif time_bound == "no":
             message = "But you have time to finish the task."
-            print(f"{reminder}. {message}")
+            print(f"Reminder: {reminder}. {message}")
     case "medium":
         if time_bound == "yes":
             message = "that requires attention, time is running out."
-            print(f"{reminder} {message}")
+            print(f"Reminder: {reminder} {message}")
         elif time_bound == "no":
             message = "But you can complete it in your free time."
-            print(f"{reminder}. {message}")
+            print(f"Reminder: {reminder}. {message}")
     case "low":
         if time_bound == "yes":
             message = "Time is running out!"
-            print(f"{reminder}. {message}")
+            print(f"Reminder: {reminder}. {message}")
         elif time_bound == "no":
             message = "Consider completing it in your free time."
-            print(f"{reminder}. {message}")
+            print(f"Reminder: {reminder}. {message}")
     case _:
         reminder = f"incorrect input!"
         print(reminder)
