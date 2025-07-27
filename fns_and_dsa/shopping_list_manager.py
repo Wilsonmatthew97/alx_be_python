38cad4d6faa0efecb2da06e5e2b5060b851bd0f3
wilson <wilsonedet96@gmail.com>
2025-07-27 20:15:28 +0100

    shopping list

--- fns_and_dsa/shopping_list_manager.py
+++ fns_and_dsa/shopping_list_manager.py
@@ -12,10 +12,10 @@ def main():
         choice = int(input("Enter your choice: "))
 
         if choice == 1:
-            item = input("Enter an item to add")
+            item = input("Enter the item to add")
             shopping_list.append(item)
         elif choice == 2:
-            item = input("Enter an item to remove")
+            item = input("Enter the item to remove")
             shopping_list.remove(item)
         elif choice == 3:
             print(shopping_list)
