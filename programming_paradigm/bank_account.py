a1e65602e837f3cc8871402659d47dc55b5a5e6f
wilson <wilsonedet96@gmail.com>
2025-08-03 15:41:48 +0100

    bank account

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -12,4 +12,4 @@ class BankAccount:
         return True
     
     def display_balance(self):
-        print("Current balance:", self.account_balance)
+        print("Current Balance:", self.account_balance)
