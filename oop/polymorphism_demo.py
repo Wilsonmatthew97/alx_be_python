592cb736027b3838014803f815fcacb90ad7ca5d
wilson <wilsonedet96@gmail.com>
2025-08-11 00:15:45 +0100

    polymorphism

---
+++ oop/polymorphism_demo.py
@@ -0,0 +1,20 @@
+import math 
+
+class Shape:
+    def area(self):
+        raise NonImplementedError("Derived class need to overide area() method.")
+
+class Rectangle(Shape):
+    def __init__(self, width, length):
+        self.length = length
+        self.width = width
+
+    def area(self):
+        return  self.length * self.width
+
+class Circle(Shape):
+    def __init__(self, radius):
+        self.radius = radius
+
+    def area(self):
+        return math.pi * (self.radius ** 2)
